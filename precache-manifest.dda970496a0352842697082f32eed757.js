fca636c4ee506fe2bb97a0cd08121f7b11b2017f
Artur Augusto Martins <arturaugusto@gmail.com>
2020-04-17 11:22:30 -0300

    test

---
+++ precache-manifest.dda970496a0352842697082f32eed757.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e75d8b1d89b7fa2de0b",
+    "url": "/css/app.9c077795.css"
+  },
+  {
+    "revision": "dda0ef2161965f482f63",
+    "url": "/css/chunk-vendors.e35cb136.css"
+  },
+  {
+    "revision": "5e61838f35fb03808eefac14cd0126b1",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "81ede6a4a184b81dbcc2c09630b00d7e",
+    "url": "/img/200_118_versaopositiva.81ede6a4.png"
+  },
+  {
+    "revision": "1d344979a449f9fd561b27d22376876c",
+    "url": "/img/phone.1d344979.png"
+  },
+  {
+    "revision": "8665e56f7947f696dcb39780614d3ec4",
+    "url": "/img/thumb_certificado_full.8665e56f.png"
+  },
+  {
+    "revision": "29015fac520f0230303c209756826573",
+    "url": "/img/thumb_ocorrencias_full.29015fac.png"
+  },
+  {
+    "revision": "ffacb39190177a3d86ade27e8e0e1549",
+    "url": "/img/thumb_orcamento_full.ffacb391.png"
+  },
+  {
+    "revision": "3a0690753c08ca9bab22e46de657a1ee",
+    "url": "/img/thumb_tablet_full.3a069075.png"
+  },
+  {
+    "revision": "a2d996ad2256db5bb28a36a7529bad88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e75d8b1d89b7fa2de0b",
+    "url": "/js/app.173e680a.js"
+  },
+  {
+    "revision": "dda0ef2161965f482f63",
+    "url": "/js/chunk-vendors.2e6ecb95.js"
+  },
+  {
+    "revision": "a26d0df6fd20572fa17d90df731f654f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
